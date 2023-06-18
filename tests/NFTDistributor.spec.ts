21dc322550b662b84ea7ef1e430041e8853f2eb3
1ixi1 <https://1ixi1@github.com>
2023-06-19 00:38:24 +0300

    distributing jettons

--- tests/NFTDistributor.spec.ts
+++ tests/NFTDistributor.spec.ts
@@ -1,6 +1,6 @@
 import { Blockchain, SandboxContract, TreasuryContract, BlockchainSnapshot, printTransactionFees, internal } from '@ton-community/sandbox';
 import { Address, Cell, toNano, Dictionary, beginCell } from 'ton-core';
-import { PayoutCollection, Errors, Op } from '../wrappers/PayoutNFTCollection';
+import { PayoutCollection, Errors, Op, Distribution } from '../wrappers/PayoutNFTCollection';
 import { PayoutItem } from '../wrappers/PayoutNFTItem';
 import { JettonMinter as DAOJettonMinter } from '../contracts/jetton_dao/wrappers/JettonMinter';
 import { JettonWallet as PoolJettonWallet } from '../wrappers/JettonWallet';
@@ -24,6 +24,8 @@ describe('Distributor NFT Collection', () => {
     let dao_minter_code: Cell;
     let dao_wallet_code: Cell;
     let totalBill: bigint;
+    let initDistribution: Distribution;
+    let jwalletAddr: Address
 
     beforeAll(async () => {
         blockchain = await Blockchain.create();
@@ -51,22 +53,76 @@ describe('Distributor NFT Collection', () => {
                                                   content: Cell.EMPTY,
                                                   voting_code: dao_voting_code},
                                                   dao_minter_code));
+        // to use it in all "describe" blocks
+        await poolJetton.sendDeploy(deployer.getSender(), toNano("10"));
+        jwalletAddr = await poolJetton.getWalletAddress(collection.address);
+
+        snapshots = new Map<string, BlockchainSnapshot>();
+        shares = new Map<Address, bigint>();
+        totalBill = 0n;
+        for (let addr of [deployer.address, notDeployer.address, randomAddress()]) {
+            const share = getRandomTon(1, 100000);
+            totalBill += share;
+            shares.set(addr, share);
+        }
     });
 
-    describe("Distributing TONs", () => {
-        const initDistribution = {
-            active: false,
-            isJetton: false,
-            volume: 0n
+    async function deploy() {
+        await loadSnapshot("uninitialized");
+        const deployResult = await collection.sendDeploy(deployer.getSender(), initDistribution, toNano("1"));
+        expect(deployResult.transactions).toHaveTransaction({
+            to: collection.address,
+            success: true,
+            endStatus: 'active'
+        });
+        snapshots.set("initialized", blockchain.snapshot());
+    }
+    async function mint() {
+        await loadSnapshot("initialized");
+        const dataBefore = await collection.getCollectionData();
+        let index = dataBefore.nextItemIndex;
+        for (let [addr, share] of shares) {
+            const mintResult = await collection.sendMint(deployer.getSender(), addr, share);
+            const nftAddress = await collection.getNFTAddress(index);
+            expect(mintResult.transactions).toHaveTransaction({
+                to: collection.address,
+                success: true,
+                outMessagesCount: 1
+            });
+            expect(mintResult.transactions).toHaveTransaction({
+                from: collection.address,
+                to: nftAddress,
+                success: true,
+                deploy: true
+            });
+            expect(mintResult.transactions).toHaveTransaction({
+                from: nftAddress,
+                to: addr,
+                op: Op.ownership_assigned
+            });
+            index++;
         }
+        const dataAfter = await collection.getCollectionData();
+        expect(dataAfter.nextItemIndex).toEqual(index);
+        const bill = await collection.getTotalBill();
+        expect(bill).toEqual(totalBill);
+        snapshots.set("minted", blockchain.snapshot());
+    }
+    async function needInit() {
+        await loadSnapshot("uninitialized");
+        const mintResult = await collection.sendMint(deployer.getSender(), randomAddress(), getRandomTon(1, 100));
+        expect(mintResult.transactions).toHaveTransaction({
+            to: collection.address,
+            success: false,
+            exitCode: Errors.need_init
+        });
+    }
+    describe("Distributing TONs", () => {
         beforeAll(async () => {
-            snapshots = new Map<string, BlockchainSnapshot>();
-            shares = new Map<Address, bigint>();
-            totalBill = 0n;
-            for (let addr of [deployer.address, notDeployer.address, randomAddress()]) {
-                const share = getRandomTon(1, 100);
-                totalBill += share;
-                shares.set(addr, share);
+            initDistribution = {
+                active: false,
+                isJetton: false,
+                volume: 0n
             }
         });
         it('should not deploy (init) not from admin', async () => {
@@ -78,47 +134,11 @@ describe('Distributor NFT Collection', () => {
             });
             snapshots.set("uninitialized", blockchain.snapshot());
         });
-        it("should deploy collection with ton distribution", async () => {
-            await loadSnapshot("uninitialized");
-            const deployResult = await collection.sendDeploy(deployer.getSender(), initDistribution, toNano("1"));
-            expect(deployResult.transactions).toHaveTransaction({
-                to: collection.address,
-                success: true,
-                endStatus: 'active'
-            });
-            snapshots.set("initialized", blockchain.snapshot());
-        });
-        it("should mint NFT", async () => {
-            await loadSnapshot("initialized");
-            const dataBefore = await collection.getCollectionData();
-            let index = dataBefore.nextItemIndex;
-            for (let [addr, share] of shares) {
-                const mintResult = await collection.sendMint(deployer.getSender(), addr, share);
-                const nftAddress = await collection.getNFTAddress(index);
-                expect(mintResult.transactions).toHaveTransaction({
-                    to: collection.address,
-                    success: true,
-                    outMessagesCount: 1
-                });
-                expect(mintResult.transactions).toHaveTransaction({
-                    from: collection.address,
-                    to: nftAddress,
-                    success: true,
-                    deploy: true
-                });
-                expect(mintResult.transactions).toHaveTransaction({
-                    from: nftAddress,
-                    to: addr,
-                    op: Op.ownership_assigned
-                });
-                index++;
-            }
-            const dataAfter = await collection.getCollectionData();
-            expect(dataAfter.nextItemIndex).toEqual(index);
-            const bill = await collection.getTotalBill();
-            expect(bill).toEqual(totalBill);
-            snapshots.set("minted", blockchain.snapshot());
-        });
+
+        it("should deploy collection with ton distribution", deploy);
+
+        it("should mint NFT", mint);
+
         it("should not mint not from admin", async () => {
             await loadSnapshot("initialized");
             const mintResult = await collection.sendMint(notDeployer.getSender(), randomAddress(), getRandomTon(1, 100));
@@ -128,15 +148,8 @@ describe('Distributor NFT Collection', () => {
                 exitCode: Errors.unauthorized_mint_request
             });
         });
-        it("should not mint if uninitialized", async () => {
-            await loadSnapshot("uninitialized");
-            const mintResult = await collection.sendMint(deployer.getSender(), randomAddress(), getRandomTon(1, 100));
-            expect(mintResult.transactions).toHaveTransaction({
-                to: collection.address,
-                success: false,
-                exitCode: Errors.need_init
-            });
-        });
+        it("should not mint if uninitialized", needInit);
+
         it('should deploy item not from admin with failed init', async () => {
             await loadSnapshot("minted");
             const mintBody = PayoutCollection.mintMessage(notDeployer.address, getRandomTon(1, 100));
@@ -160,7 +173,8 @@ describe('Distributor NFT Collection', () => {
                 endStatus: 'active',
                 exitCode: Errors.unauthorized_init
             });
-
+            const bill = await collection.getTotalBill();
+            expect(bill).toEqual(totalBill);
             const nftSmc = await blockchain.getContract(nftItem.address);
             if (nftSmc.accountState?.type === 'active') {
                 const initedBit = nftSmc.accountState?.state.data?.beginParse().loadBit();
@@ -224,7 +238,6 @@ describe('Distributor NFT Collection', () => {
         it("should not start distribution of jettons", async () => {
             await loadSnapshot("minted");
             const mintAssetResult = await poolJetton.sendMint(deployer.getSender(), collection.address, toNano(1000), toNano("0.1"), toNano("0.5"))
-            const jwalletAddr = await poolJetton.getWalletAddress(collection.address);
             expect(mintAssetResult.transactions).toHaveTransaction({ // internal transfer
                 from: poolJetton.address,
                 to: jwalletAddr,
@@ -267,18 +280,68 @@ describe('Distributor NFT Collection', () => {
                 i++;
             }
             const billAfter = await collection.getTotalBill();
+            expect(billAfter).toEqual(0n);
             const collectionData = await collection.getDistribution();
             expect(collectionData.active).toEqual(true);
-            snapshots.set("distribution_started", blockchain.snapshot());
+            snapshots.set("distributed", blockchain.snapshot());
         });
-        it("should not start distribution if already started", async () => {
-            await loadSnapshot("distribution_started");
+        it("should not distribute again", async () => {
+            await loadSnapshot("distributed");
             const sendStartResult = await collection.sendStartDistribution(deployer.getSender(), toNano(1000));
             expect(sendStartResult.transactions).toHaveTransaction({
                 to: collection.address,
                 success: false,
                 exitCode: Errors.distribution_already_started
             });
+        });
+    });
+    describe("Distributing Jettons", () => {
+        beforeAll(async () => {
+            initDistribution = {
+                active: false,
+                isJetton: true,
+                volume: 0n,
+                myJettonWallet: jwalletAddr
+            }
+        });
+        it("should deploy collection with jetton distribution", deploy);
+        it("should mint NFT", mint)
+        it("should not start distribution of TONs", async () => {
+            const sendStartResult = await collection.sendStartDistribution(deployer.getSender(), toNano(1000));
+            expect(sendStartResult.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: collection.address,
+                success: false,
+                exitCode: Errors.cannot_distribute_tons
+            });
+        });
+        it("should distribute jettons", async () => {
+            const assetAmount = getRandomTon(100, 10000);
+            const billBefore = await collection.getTotalBill();
+            const mintAssetResult = await poolJetton.sendMint(deployer.getSender(), collection.address, assetAmount, toNano("0.1"), toNano("0.5"))
+            expect(mintAssetResult.transactions).toHaveTransaction({
+                from: jwalletAddr,
+                to: collection.address,
+                op: Op.transfer_notification,
+                success: true
+            });
+            for (let [addr, share] of shares) {
+                const userWalletAddr = await poolJetton.getWalletAddress(addr);
+                const expectedJettonsRecieved = assetAmount * share / billBefore;
+                expect(mintAssetResult.transactions).toHaveTransaction({
+                    from: userWalletAddr,
+                    to: addr,
+                    op: Op.transfer_notification,
+                    body: (x) => {x!
+                        let cs = x.beginParse().skip(32 + 64)
+                        let jetton_amount = cs.loadCoins()
+                        let addr_serialization = cs.loadUint(2)
+                       return (expectedJettonsRecieved + 5n >= jetton_amount)
+                           || (jetton_amount >= expectedJettonsRecieved - 5n)
+                           && (addr_serialization == 0b00) // recieved from null address
+                    }
+                });
+            }
         });
     });
 });
