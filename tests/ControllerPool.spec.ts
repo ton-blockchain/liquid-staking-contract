8d67461c109d8f1ee8f8f3b644d78511fac4ea46
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2023-06-29 18:21:43 +0300

    Use types instead of store_uint/load_uint where applicable

--- tests/ControllerPool.spec.ts
+++ tests/ControllerPool.spec.ts
@@ -232,7 +232,7 @@ describe('Controller & Pool', () => {
             });
             const loan = await pool.getLoan(0, deployer.address);
             const { interestRate } = await pool.getFinanceData()
-            const expectedInterest = loanRequestParams[1] * BigInt(interestRate) / 65535n;
+            const expectedInterest = loanRequestParams[1] * BigInt(interestRate) / 65536n;
             expect(loan.borrowed).toEqual(loanRequestParams[1]);
             expect(loan.interestAmount).toEqual(expectedInterest);
         });
