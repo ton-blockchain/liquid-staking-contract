d82be6d4bd7a9f16d0faa3813428f1272ca53752
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2023-07-16 17:36:47 +0300

    Add versioning

--- wrappers/Controller.compile.ts
+++ wrappers/Controller.compile.ts
@@ -1,6 +1,17 @@
 import { CompilerConfig } from '@ton-community/blueprint';
+import { readFile, writeFile, mkdir } from 'fs/promises';
+import path from 'path';
 
 export const compile: CompilerConfig = {
     lang: 'func',
-    targets: ['contracts/controller.func'],
+    preCompileHook: async () => {
+        const path_to_head = (await readFile(path.join(__dirname, '..', '.git', 'HEAD'), "utf-8")).split(": ")[1].trim();
+        const head = await readFile(path.join(__dirname, '..', '.git', path_to_head), "utf-8");
+
+        const auto = path.join(__dirname, '..', 'contracts', 'auto');
+        await mkdir(auto, { recursive: true });
+        await writeFile(path.join(auto, 'git-hash.func'), `const int git_hash = 0x${head.trim()};`);
+    },
+    targets: ['contracts/versioning.func',
+              'contracts/controller.func'],
 };
---
+++ wrappers/Librarian.ts
@@ -0,0 +1,38 @@
+import { Address, toNano, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, Message, storeMessage } from 'ton-core';
+
+
+export type LibrarianConfig = {
+  librarianId: bigint;
+};
+
+export function librarianConfigToCell(config: LibrarianConfig): Cell {
+    return beginCell()
+              .storeUint(config.librarianId, 32)
+           .endCell();
+}
+export class Librarian implements Contract {
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+
+    static createFromAddress(address: Address) {
+        return new Librarian(address);
+    }
+
+    static createFromConfig(config: LibrarianConfig, code: Cell, workchain = -1) {
+        const data = librarianConfigToCell(config);
+        const init = { code, data };
+        return new Librarian(contractAddress(workchain, init), init);
+    }
+
+    async sendAddLibrary(provider: ContractProvider, via: Sender, code:Cell, value:bigint = toNano('50')) {
+        await provider.internal(via, {
+            value: value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                     .storeUint(0x7f567a32, 32) // op = top up
+                     .storeUint(0, 64) // query id
+                     .storeUint(2000, 32) // query id
+                     .storeRef(code)
+                  .endCell(),
+        });
+    }
+}
