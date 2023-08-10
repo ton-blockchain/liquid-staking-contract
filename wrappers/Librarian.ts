8519d3ba31be50af256151ba857f2e65eec9e362
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2023-08-10 18:23:22 +0300

    Add Librarian contract/wrapper

--- wrappers/Librarian.ts
+++ wrappers/Librarian.ts
@@ -13,6 +13,13 @@ export function librarianConfigToCell(config: LibrarianConfig): Cell {
 export class Librarian implements Contract {
     constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
 
+    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY
+        });
+    }
+
     static createFromAddress(address: Address) {
         return new Librarian(address);
     }
@@ -23,14 +30,14 @@ export class Librarian implements Contract {
         return new Librarian(contractAddress(workchain, init), init);
     }
 
-    async sendAddLibrary(provider: ContractProvider, via: Sender, code:Cell, value:bigint = toNano('50')) {
+    async sendAddLibrary(provider: ContractProvider, via: Sender, code:Cell, value:bigint = toNano('250')) {
         await provider.internal(via, {
             value: value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell()
                      .storeUint(0x7f567a32, 32) // op = top up
                      .storeUint(0, 64) // query id
-                     .storeUint(2000, 32) // query id
+                     .storeUint(2000, 32) // max_cells
                      .storeRef(code)
                   .endCell(),
         });
