1d16db5a528f50ef78bd4b4026cbc1070e5b78ba
Trinketer22 <trinketer22@localhost>
2024-01-04 18:37:24 +0300

    UI wrappers

--- scripts/deployPool.ts
+++ scripts/deployPool.ts
@@ -1,42 +1,12 @@
 import { Address, Cell, toNano, beginCell } from '@ton/core';
-import { Pool, dataToFullConfig, poolFullConfigToCell } from '../wrappers/Pool';
+import { Pool, dataToFullConfig, poolFullConfigToCell, PoolFullConfig } from '../wrappers/Pool';
 import { PoolState } from "../PoolConstants";
 import { JettonMinter as DAOJettonMinter, jettonContentToCell } from '../contracts/jetton_dao/wrappers/JettonMinter';
 import { compile, NetworkProvider, sleep } from '@ton/blueprint';
 import {JettonWallet as PoolJettonWallet } from '../wrappers/JettonWallet';
 import { Controller } from '../wrappers/Controller';
 import { Librarian, LibrarianConfig } from '../wrappers/Librarian';
-
-
-const waitForTransaction = async (provider:NetworkProvider, address:Address,
-                                  action:string = "transaction",
-                                  curTxLt:string | null = null,
-                                  maxRetry:number = 15,
-                                  interval:number=1000) => {
-    let done  = false;
-    let count = 0;
-    const ui  = provider.ui();
-    let blockNum = (await provider.api().getLastBlock()).last.seqno;
-    if(curTxLt == null) {
-        let initialState = await provider.api().getAccount(blockNum, address);
-        let lt = initialState?.account?.last?.lt;
-        curTxLt = lt ? lt : null;
-    }
-    do {
-        ui.write(`Awaiting ${action} completion (${++count}/${maxRetry})`);
-        await sleep(interval);
-        let newBlockNum = (await provider.api().getLastBlock()).last.seqno;
-        if (blockNum == newBlockNum) {
-            continue;
-        }
-        blockNum = newBlockNum;
-        const curState = await provider.api().getAccount(blockNum, address);
-        if(curState?.account?.last !== null){
-            done = curState?.account?.last?.lt !== curTxLt;
-        }
-    } while(!done && count < maxRetry);
-    return done;
-}
+import { waitForTransaction } from '../wrappers/ui-utils';
 
 export async function run(provider: NetworkProvider) {
 
